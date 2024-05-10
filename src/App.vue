d55c641642bd3d600f86a973e1129a2267395342
wajwajwaj <929427675@qq.com>
2024-05-10 17:41:15 +0800

    demo 配置格式化参数

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <add-component></add-component>
+    <h5>-------------------</h5>
+    <reduce-component></reduce-component>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import AddComponent from './components/AddComponent.vue'
+import ReduceComponent from './components/ReduceComponent.vue'
+export default {
+  data () {},
+  components: {
+    'add-component': AddComponent,
+    'reduce-component': ReduceComponent
+  }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
---
+++ src/components/ReduceComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span>当前count的值是：</span>
+    <button>-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {}
+}
+</script>
+
+<style scoped>
+</style>
